d4dcae14c94178d99222cb20d966bb14c1524020
DwkLoki <dwiky.darmawansyah@gmail.com>
2025-08-13 21:51:10 +0700

    buat desain responsive untuk landing page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,93 @@
 import navLogo from '../assets/icons/navbar-logo.svg'
 import { NavLink } from 'react-router-dom'
+import { X, AlignJustify } from 'lucide-react';
+import { useState } from 'react';
 
 export default function NavBar(props) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     const navLinkClass = ({ isActive }) => isActive ? 'text-[#2C448C]' : 'hover:text-[#2C448C]'
-    
+
     return (
-        <nav className={`flex h-20 justify-between items-center px-20 ${props.color}`}>
-            <img src={navLogo} alt="Navbar Logo" />
-            <ul className='flex items-center space-x-12 text-lg'>
-                <li>
-                    <NavLink to='/' className={navLinkClass}>
-                        Beranda
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/about-us' className={navLinkClass}>
-                        Tentang Kami
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/login' className={navLinkClass}>
-                        Masuk
-                    </NavLink>
-                </li>
-                <li className='border rounded-xl bg-[#2C448C] w-28 py-1 text-center text-white font-bold'>
-                    <NavLink to='/register'>
-                        Daftar
-                    </NavLink>
-                </li>
-            </ul>
-        </nav> 
+        <nav>
+            <div className={`flex h-20 justify-between items-center lg:px-20 md:px-10 px-5 ${props.color}`}>
+                <img src={navLogo} alt="Navbar Logo" />
+                <ul className='md:flex hidden items-center lg:space-x-12 md:space-x-6 text-lg'>
+                    <li>
+                        <NavLink to='/' className={navLinkClass}>
+                            Beranda
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about-us' className={navLinkClass}>
+                            Tentang Kami
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/login' className={navLinkClass}>
+                            Masuk
+                        </NavLink>
+                    </li>
+                    <li className='border rounded-xl bg-[#2C448C] w-28 py-1 text-center text-white font-bold'>
+                        <NavLink to='/register'>
+                            Daftar
+                        </NavLink>
+                    </li>
+                </ul>
+                <button
+                    // ref={props.toggleButtonRef} // pasang ref
+                    // onClick={(e) => {
+                    //     e.stopPropagation(); // cegah bubbling
+                    //     props.sidebarToggle();
+                    // }}
+                    onClick={() => setIsMenuOpen(prev => !prev)}
+                    className='md:hidden relative w-8 h-8 flex items-center justify-center'
+                >
+                    {/* Icon Menu */}
+                    <span
+                        className={`absolute transition-all duration-200 ease-in-out ${isMenuOpen ? 'opacity-0 scale-75 rotate-90' : 'opacity-100 scale-100 rotate-0'}`}
+                    >
+                        <AlignJustify size={24} strokeWidth={2} color="#2C448C" />
+                    </span>
+
+                    {/* Icon Close */}
+                    <span
+                        className={`absolute transition-all duration-200 ease-in-out ${isMenuOpen ? 'opacity-100 scale-100 rotate-0' : 'opacity-0 scale-75 -rotate-90'}`}
+                    >
+                        <X size={24} strokeWidth={2} color="#2C448C" />
+                    </span>
+                </button>
+            </div>
+
+            {/* toggle menu pada tampilan mobile */}
+            <div
+                className={`
+                    md:hidden w-screen overflow-hidden transition-all duration-500 ease-in-out
+                    ${isMenuOpen ? 'h-[calc(100vh-80px)] opacity-100' : 'h-0 opacity-0'}
+                `}
+            >
+                <ul className='flex flex-col gap-4 h-full justify-center items-center text-lg'>
+                    <li>
+                        <NavLink to='/' className={navLinkClass}>
+                            Beranda
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about-us' className={navLinkClass}>
+                            Tentang Kami
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/login' className={navLinkClass}>
+                            Masuk
+                        </NavLink>
+                    </li>
+                    <li className='border rounded-xl bg-[#2C448C] w-28 py-1 text-center text-white font-bold'>
+                        <NavLink to='/register'>
+                            Daftar
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,56 +15,56 @@ export default function Home() {
             </header>
 
             <main>
-                <section className='flex flex-col mx-6 sm:flex-row items-center sm:space-x-12 sm:mx-20 py-10'>
-                    <img src={heroImg} alt="Hero Image 1" className='hidden sm:block'/>
-                    <div>
-                        <h1 className='text-2xl sm:text-4xl font-bold'>Bingung ngerjain tugas kuliah?</h1>
-                        <p className='text-base pt-4 pb-6 sm:text-xl sm:pt-6 sm:pb-8'>Tenang, Betewe siap membantu kamu mendapatkan informasi dan pembahasan sesuai topik perkuliahanmu!</p>
-                        <div className='flex items-center space-x-4 w-full h-[44px] sm:w-[415px] sm:h-[55px] bg-white rounded-2xl pl-6 pr-2'>
-                            <input type="text" placeholder='Cari jawaban dari pertanyaanmu' className='flex-1 outline-none'/>
-                            <button className='w-[25px] h-[25px] sm:w-[30px] sm:h-[30px] bg-[#84ACF8] rounded-full flex items-center justify-center'>
-                                <Search className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
+                <section className='flex w-full items-center gap-12 lg:px-20 sm:px-10 px-5 py-10'>
+                    <img src={heroImg} alt="Hero Image 1" className='w-1/2 hidden md:block' />
+                    <div className='md:w-1/2 w-full'>
+                        <h1 className='lg:text-4xl text-2xl font-bold'>Bingung ngerjain tugas kuliah?</h1>
+                        <p className='lg:text-xl text-base lg:pt-6 lg:pb-8 pt-3 pb-4'>Tenang, Betewe siap membantu kamu mendapatkan informasi dan pembahasan sesuai topik perkuliahanmu!</p>
+                        <div className='flex items-center lg:space-x-4 space-x-3 w-full h-[44px] lg:w-[415px] lg:h-[55px] bg-white rounded-2xl lg:pl-6 pl-3 pr-2 focus-within:outline focus-within:outline-[#84ACF8]'>
+                            <input type="text" placeholder='Cari jawaban dari pertanyaanmu' className='flex-1 outline-none' />
+                            <button className='w-[25px] h-[25px] lg:w-[30px] lg:h-[30px] bg-[#84ACF8] rounded-full flex items-center justify-center'>
+                                <Search className="w-4 h-4 lg:w-5 lg:h-5 text-white" />
                             </button>
                         </div>
                     </div>
                 </section>
 
-                <section className='mx-20 pt-20'>
-                    <h1 className='text-4xl font-bold text-center'>
+                <section className='w-full lg:px-20 sm:px-10 px-5 py-10'>
+                    <h1 className='lg:text-4xl text-2xl font-bold text-center'>
                         Temukan jawaban dari segala <br />
                         <span className="relative inline-block pb-4">
                             pertanyaanmu!
                             <span className="absolute left-1/2 -translate-x-1/2 bottom-0 w-4/5 h-[4px] bg-[#84ACF8] rounded-sm"></span>
                         </span>
                     </h1>
-                    <div className='flex flex-col space-y-20 my-20'>
-                        <div className='flex justify-between items-center'>
-                            <div className='w-[470px]'>
+                    <div className='flex flex-col lg:space-y-20 lg:mt-20 space-y-10 mt-10'>
+                        <div className='flex sm:flex-row flex-col-reverse lg:gap-32 md:gap-16 gap-6 items-center'>
+                            <div className='flex-1'>
                                 <p>Tiap habis kelas langsung dikasih tugas dan deadline-nya mepet? Cari materi di buku perlu banyak waktu dan belum tentu nemu?</p>
                                 <p className='font-bold mt-2'>BTW, Tanya-Jawab yuk!</p>
                             </div>
-                            <img src={contentImg1} alt="Gambar Konten Pertama" />
+                            <img className='sm:w-[45%] w-4/5' src={contentImg1} alt="Gambar Konten Pertama" />
                         </div>
-                        <div className='flex justify-between items-center'>
-                            <img src={contentImg2} alt="Gambar Konten Kedua" />
-                            <div className='w-[470px]'>
+                        <div className='flex sm:flex-row flex-col lg:gap-32 md:gap-16 gap-6 items-center'>
+                            <img className='sm:w-[45%] w-4/5' src={contentImg2} alt="Gambar Konten Kedua" />
+                            <div className='flex-1'>
                                 <p>Kamu suka membahas sebuah topik materi dengan teman-teman lain untuk membantumu mendapat pemahaman yang lebih baik?</p>
                                 <p className='font-bold mt-2'>BTW, Diskusi bareng yuk!</p>
                             </div>
                         </div>
-                        <div className='flex justify-between items-center'>
-                            <div className='w-[470px]'>
+                        <div className='flex sm:flex-row flex-col-reverse lg:gap-32 md:gap-16 gap-6 items-center'>
+                            <div className='flex-1'>
                                 <p>Kesulitan cari referensi berdasarkan program studi-mu? Search by keyword tetapi hasilnya masih belum memuaskan?</p>
                                 <p className='font-bold mt-2'>BTW, kategorinya sesuai jurusan loh!</p>
                             </div>
-                            <img src={contentImg3} alt="Gambar Konten Ketiga" />
+                            <img className='sm:w-[45%] w-4/5' src={contentImg3} alt="Gambar Konten Ketiga" />
                         </div>
                     </div>
                 </section>
 
-                <section className='py-20'>
-                    <h1 className='text-4xl font-bold text-center'>
-                        <span className="relative inline-block pb-4 mb-12">
+                <section className='w-full py-10'>
+                    <h1 className='lg:text-4xl text-2xl font-bold text-center'>
+                        <span className="relative inline-block pb-4 lg:mb-12 mb-6">
                             Baca pendapat mereka yuk!
                             <span className="absolute left-1/2 -translate-x-1/2 bottom-0 w-2/5 h-[4px] bg-[#84ACF8] rounded-sm"></span>
                         </span>
@@ -72,11 +72,11 @@ export default function Home() {
                     <TestiCarousel />
                 </section>
 
-                <section className='flex items-end space-x-12 mx-20 pb-20'>
-                    <img src={newsLetterImg} alt="Gambar Newsletter" />
-                    <div className='flex flex-col space-y-6'>
-                        <h1 className='text-4xl font-bold'>Daftarkan e-mail kamu untuk mendapatkan info terbaru dari kami, GRATIS!</h1>
-                        <div className='flex items-center space-x-4 w-[415px] h-[55px] bg-white rounded-2xl px-6'>
+                <section className='flex md:flex-row flex-col w-full md:items-end items-center md:gap-12 gap-6 lg:px-20 sm:px-10 px-5 py-10'>
+                    <img className='lg:w-[35%] md:w-[45%] w-4/5' src={newsLetterImg} alt="Gambar Newsletter" />
+                    <div className='flex flex-col md:items-start items-center lg:w-[65%] md:w-[55%] space-y-6'>
+                        <h1 className='lg:text-4xl text-2xl text-center md:text-left font-bold'>Daftarkan e-mail kamu untuk mendapatkan info terbaru dari kami, GRATIS!</h1>
+                        <div className='flex items-center w-full h-[44px] lg:w-[415px] lg:h-[55px] bg-white rounded-2xl px-6'>
                             <input type="text" placeholder='Tulis email kamu di sini' className='w-full outline-none' />
                         </div>
                         <button className='border rounded-xl bg-[#2C448C] w-28 py-1 text-center text-white font-bold'>
@@ -86,7 +86,7 @@ export default function Home() {
                 </section>
             </main>
 
-            <footer className='h-[150px] bg-[#2C448C] text-white flex flex-col justify-center items-center'>
+            <footer className='h-[150px] bg-[#2C448C] text-white flex flex-col justify-center items-center px-5 text-center gap-2'>
                 <p>©2025 PT Betewe Apa ya. All Rights Reserved</p>
                 <p>Tentang Kami - Ketentuan Privasi - Berlanganan - FAQ - Hubungi Kami</p>
             </footer>
